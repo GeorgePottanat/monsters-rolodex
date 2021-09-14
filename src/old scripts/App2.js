dbaa1170ea54c5ee37f185f0b7234644802bee0c
George Pottanat <pottanat.george@gmail.com>
2021-09-14 19:05:11 -0400

    adding my first website on github

---
+++ src/old scripts/App2.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            string: 'hello Gerge!',
+            monsters: [
+                {
+                    id: '1',
+                    name: 'Frankenstein'
+                },
+                {
+                    id: '2',
+                    name: 'Dracula'
+                },
+                {
+                    id: '3',
+                    name: 'Zombie'
+                }
+            ]
+        }
+    }
+    render() {
+        return (
+            <div className="App">
+                {this.state.monsters.map(monster =>
+                    <h1 key={monster.id}>{monster.name}</h1>
+                )}
+            </div>
+        )
+    }
+}
+
+export default App;
